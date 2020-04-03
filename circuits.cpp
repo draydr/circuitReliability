203a540adca0ae789c63dbae0d7400c4a51bd907
David Dray <draydr17@gcc.edu>
2020-04-03 10:03:02 -0400

    added standard setup

---
+++ circuits.cpp
@@ -0,0 +1,27 @@
+/*
+* David Dray ~ COMP 233 ~ Spr 2020
+* File:    circuits.c
+* Purpose: test reliability of a set of circuits
+*/
+
+#define _CRT_SECURE_NO_WARNINGS //iso standard is good for us
+//include statements
+#include<stdio.h> 
+#include<stdlib.h>
+#include<omp.h>
+
+// method stubs
+
+
+int main() {
+    //variable dictionary:
+
+
+    //standard initial output
+    printf("David Dray ~ COMP 233 B ~ Spring 2020\n");
+    printf("Circuits - Test reliability of a set of circuits\n");
+
+    //standard termination message
+    printf("\n\n\t<<Normal Termination>>\n\n");
+    return 0;
+}
